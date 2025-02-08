6a7761e7a2eed12fe49e4afb1a6028e8acc4c4bb
rajanga <ganeshbabu.rajan@rabobank.nl>
2025-02-08 19:05:13 +0100

    input parameter to user component added git

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -11,15 +10,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  imagePath  = computed(() => {
-    return 'assets/users/' + this.selectedUser().avatar;
-  })
+  @Input() name!: string;
+  @Input() avatar!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectedUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set( DUMMY_USERS[randomIndex]);
-    console.log('user '+ this.selectedUser().name +' has been clicked');
+    console.log('user is clicked');
   }
 }
