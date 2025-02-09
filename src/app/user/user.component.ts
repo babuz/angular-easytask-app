08cc7d8920790584c7948f0f235e1993f57eb92e
rajanga <ganeshbabu.rajan@rabobank.nl>
2025-02-09 09:36:30 +0100

    user type interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+interface User {
+  name : string;
+  avatar: string;
+  id: string;
+
+}
 
 @Component({
   selector: 'app-user',
@@ -11,17 +17,18 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
 
-  @Input({required : true}) name!: string;
-  @Input({required : true}) avatar!: string;
-  @Input({required : true}) id!: string;
+  @Input({required:true}) user!: User;
+  // @Input({required : true}) name!: string;
+  // @Input({required : true}) avatar!: string;
+  // @Input({required : true}) id!: string;
 
   @Output() select = new EventEmitter<string>()
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectedUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
